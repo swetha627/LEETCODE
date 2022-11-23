7d48c9a7dada66f47da52ec9d05985091793484d
swetha g <74397685+swetha627@users.noreply.github.com>
2022-11-23 16:37:17 +0530

    Time: 79 ms (23.50%), Space: 13.8 MB (98.65%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,6 +1,15 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        for i in t:
-            if s.count(i)!=t.count(i):
-                return i
+        ds = {}
+    
+        for i in range(len(s)):
+            ds[s[i]] = 1 + ds.get(s[i],0)
+
+        dt = {}
+        for i in range(len(t)):
+            dt[t[i]] = 1 + dt.get(t[i], 0)
+            
+        for char in t:
+            if char not in ds or dt[char] != ds[char]:
+                return char
         
